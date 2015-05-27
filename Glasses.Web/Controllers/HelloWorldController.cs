8cd9ad0089b4a70a61b62750c8f4db00371c00d8
Andrewlionki14 <andrew.lion.ki14@gmail.com>
2015-05-27 15:24:49 +0300

    added HelloWorldController, changed RouteConfig

---
+++ Glasses.Web/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int dptr = 0)
+        {
+            return HttpUtility.HtmlEncode("Glasses " + name + ",diopter: +" + dptr);
+        }
+
+     
+    }
+}
